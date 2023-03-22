92f4b5369ce912142d16d8aeeaeccea6a5d307c9
Classy <69457423+Classy08@users.noreply.github.com>
2023-03-22 11:52:10 +0530

    Update test.js

--- venge/scripts/test.js
+++ venge/scripts/test.js
@@ -1,4 +1,16 @@
-class Store {
+
+window._pc = false;
+Object.defineProperty(window, "pc", {
+    set(value) {
+        if (!window.pc) {
+            window._pc = value;
+        }
+    },
+    get() {
+        return (window._pc);
+    }
+});
+class localStore {
     constructor(config) {
         this.config = JSON.parse(config);
     }
@@ -7,7 +19,7 @@ class Store {
     set = (key, value) => {
         this.config[key] = value;
         localStorage.saveSettings = JSON.stringify(this.config);
-    }a
+    }
 }
 
 if (!localStorage.saveSettings) localStorage.saveSettings = JSON.stringify({
@@ -19,7 +31,7 @@ if (!localStorage.saveSettings) localStorage.saveSettings = JSON.stringify({
 })
 var setting_array = ['HideHands', 'HideADS', 'ToggleWeapon', 'DisableLight', 'BetterUI']
 
-const settings = new Store(localStorage.saveSettings);
+const localsettings = new localStore(localStorage.saveSettings);
 
     new MutationObserver(mutationRecords => {
         try {
@@ -62,10 +74,6 @@ const settings = new Store(localStorage.saveSettings);
                             <input type="checkbox" class="settingToggle" id="ToggleWeapon">
                         </div>
                         <div class="settingCont">
-                            Disable Map Lighting
-                            <input type="checkbox" class="settingToggle" id="DisableLight">
-                        </div>
-                        <div class="settingCont">
                             Better Ingame UI
                             <input type="checkbox" class="settingToggle" id="BetterUI">
                         </div>
@@ -75,12 +83,12 @@ const settings = new Store(localStorage.saveSettings);
 
                         tabCont.onclick = (e) => {
                             if (e.target.classList.contains("settingToggle")) {
-                                settings.set(e.target.id, e.target.checked)
+                                localsettings.set(e.target.id, e.target.checked)
                             }
                         }
                     
                         setting_array.forEach(name => {
-                            document.getElementById(name).checked = settings.get(name)
+                            document.getElementById(name).checked = localsettings.get(name)
                         });
                         
                     }
@@ -92,24 +100,21 @@ const settings = new Store(localStorage.saveSettings);
     }).observe(document, { childList: true, subtree: true });
     
 document.addEventListener("DOMContentLoaded", () => {
-    window.pc.app.once('start', () => {
-
-    })
     pc.app.on('Player:Focused', function (state) {
         try {
             if (state) {
-                if (settings.get('HideADS')) {
+                if (localsettings.get('HideADS')) {
                     pc.app.scene.layers.getLayerByName('NonFOV').enabled = false;
                     let crosshair = pc.app.root.findByName('Crosshair');
                     crosshair.enabled = true;
                 }
             } else {
-                if (settings.get('HideHands')) {
+                if (localsettings.get('HideHands')) {
                     pc.app.root.findByName('Player').findByName('ArmLeft').enabled = false;
                     pc.app.root.findByName('Player').findByName('ArmRight').enabled = false;
                 }
                 pc.app.scene.layers.getLayerByName('NonFOV').enabled = true;
-                if (settings.get('BetterUI')) {
+                if (localsettings.get('BetterUI')) {
                     pc.app.root.findByName('Overlay').findByName("Hitmarker").enabled = false;
                     pc.app.root.findByName('Overlay').findByName('Leaderboard').enabled = false;
                     pc.app.root.findByName('Overlay').findByName("Weapons").enabled = false;
@@ -121,20 +126,12 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    pc.app.on("Map:Loaded", () => {
-
-        if (settings.get('DisableLight')) {
-            pc.app.root.findByName('MapHolder').findByName('Light').light.color = {r: 0, g: 0, b: 0, a: 1};
-        } else {
-            pc.app.root.findByName('MapHolder').findByName('Light').light.color = {r: 1, g: 1, b: 1, a: 1};
-        }
-    })
-
-    if (settings.get('ToggleWeapon')) {
+    if (localsettings.get('ToggleWeapon')) {
         document.addEventListener('keydown', (e) => {
             if (e.code == 'Backquote') {
                 try {
                     pc.app.root.findByName('Weapon').enabled = !pc.app.root.findByName('Weapon').enabled;
+                    pc.app.scene.layers.getLayerById(1000).enabled = !pc.app.scene.layers.getLayerById(1000).enabled;
                 } catch (error) {
                     console.log('not in a game')
                 }
